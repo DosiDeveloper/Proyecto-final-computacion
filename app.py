c6de3fd5d3c2f28eb671783e4f36f536f52b6645
Douglas S Barreto B <dosideveloper@gmail.com>
2026-03-02 22:19:38 -0400

    Actualizar app.py: Limpieza de datos
    Actualiza app.py: División y extracción de las categorías en el df
    
    app.py: Eliminación de outlier en el dataframe

--- app.py
+++ app.py
@@ -14,4 +14,41 @@ def load_dataset():
 
 raw_df = load_dataset()
 
+# No hay datos duplicados, pero se eliminan por si acaso
+df = raw_df.drop_duplicates()
+
+# Eliminacion de las columnas img_link y product_link dado que existen actualmente en el server de Amazon
+df = df.drop(columns=["img_link", "product_link"])
+
+# Parse de los precios
+df["discounted_price"] = (df["discounted_price"].str.replace(
+    "₹", "")).str.replace(",", "").astype(float)
+df["actual_price"] = (df["actual_price"].str.replace(
+    "₹", "")).str.replace(",", "").astype(float)
+
+# Parse de los porcentajes de descuento
+df["discount_percentage"] = df["discount_percentage"].str.replace("%", "").astype(int)
+
+# Parse de rating
+df["rating"] = pd.to_numeric(df["rating"], errors="coerce")
+df["rating"] = df["rating"].fillna(df["rating"].dropna().median())
+
+df["rating_count"] = df["rating_count"].str.replace(",", "").fillna(
+    df["rating_count"].dropna().str.replace(",", "").astype(int).median()).astype(int)
+
+# Recalculo de los descuentos
+df["discounted_price"] = df["actual_price"] - (df["actual_price"] * (df["discount_percentage"]/100))
+
+df["category"] = df["category"].str.split(r"[|,]", regex=True)
+
+# eliminando outlier
+q1_actual_price = df["actual_price"].quantile(.25)
+q3_actual_price = df["actual_price"].quantile(.75)
+iqr_actual_price = q3_actual_price - q1_actual_price
+lim_inf_actual_price = q1_actual_price - 1.5 * iqr_actual_price
+lim_sup_actual_price = q3_actual_price + 1.5 * iqr_actual_price
+df = df.query("actual_price >= @lim_inf_actual_price and actual_price <= @lim_sup_actual_price")
+
+list_category = set()
+df["category"].apply(lambda x: list_category.update(x))
 
