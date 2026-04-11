9ec0fb9f8e58623456bee2fb6ae4a9f409de7760
Douglas S Barreto B <dosideveloper@gmail.com>
2026-04-11 16:10:14 -0400

    Revert "Cambio de graficos en la pestaña resumen y mejora en los graficos de la pestaña graficos"
    This reverts commit b8d45fc224820ec0adfb57e9eec46cc8c30bd13d.

--- fragments.py
+++ fragments.py
@@ -1,9 +1,12 @@
+from threading import local
+
+from matplotlib import legend
 import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
-import scipy.stats as stats
+from plotly.subplots import make_subplots
 
 amazon_palette = ['#00A8E1', '#ff9900',
                   '#f3ece1', '#12130F', '#232f3e', '#f2f2f2']
@@ -29,19 +32,16 @@ def resume_tab(filtered_df: pd.DataFrame, df: pd.DataFrame, rating_filter):
         col_g1, col_g2 = st.columns(2, vertical_alignment="center")
         with col_g1:
             if rating_filter != []:
-                only_cat_selected = filtered_df.query(
-                    "rating_group in @rating_filter")["rating_group"].cat.remove_unused_categories()
-                only_cat_selected.sort_values(inplace=True)
-                fig1 = px.funnel(y=only_cat_selected.cat.remove_unused_categories().unique().tolist(), 
-                                 x=only_cat_selected.cat.reorder_categories(rating_filter).value_counts()[:len(rating_filter)],
-                                 title="Conteo de productos por grupo de rating seleccionados", color_discrete_sequence=amazon_palette)
+                only_cat_selected = filtered_df.loc[filtered_df["rating_group"].isin(
+                    filtered_df["rating_group"].cat.remove_unused_categories())].groupby("rating_group", observed=True)["rating_group"].value_counts()
+                fig1 = px.pie(names=rating_filter, values=only_cat_selected,
+                              title="Conteo de productos por grupo de rating", color_discrete_sequence=amazon_palette)
             else:
-                fig1 = px.funnel(y=filtered_df["rating_group"].cat.categories.to_list(), x=filtered_df["rating_group"].value_counts(
-                ), title="Conteo de productos por grupo de rating", color_discrete_sequence=amazon_palette)
+                fig1 = px.pie(names=list(filtered_df["rating_group"].cat.categories), values=filtered_df["rating_group"].value_counts(
+                    ascending=True), title="Conteo de productos por grupo de rating", color_discrete_sequence=amazon_palette)
 
             fig1.update_traces(textposition="inside",
-                               textinfo="percent total+value")
-            fig1.update_layout(yaxis_title="Grupo de rating")
+                               textinfo="percent+label")
             st.plotly_chart(fig1)
 
         with col_g2:
@@ -62,7 +62,7 @@ def resume_tab(filtered_df: pd.DataFrame, df: pd.DataFrame, rating_filter):
         with col2:
             st.markdown(
                 "Top 10 productos con mejor rating :small_red_triangle:")
-            top_rated = df.sort_values(
+            top_rated = filtered_df.sort_values(
                 "rating", ascending=False).head(10)
             st.dataframe(top_rated[["product_name", "rating"]].rename(columns={
                 "product_name": "Nombre del producto",
@@ -83,55 +83,31 @@ def graph_tab(filtered_df: pd.DataFrame):
             "rating_count": "Conteo de rating",
             "about_product": "Descripcion del producto",
         })
-        select_dist_item = st.multiselect("Selecciona dos o una variable para graficar", [
+        select_dist_item = st.multiselect("Selecciona dos variables para graficar", [
             "Porcentaje de descuento", "Rating", "Precio real (INR)", "Precio con descuento (INR)", "Conteo de rating"], default=["Porcentaje de descuento", "Rating"], placeholder="Selecciona dos variables", max_selections=2)
         if len(select_dist_item) == 0:
             st.warning(
                 "Selecciona al menos una variable para mostrar su distribución, dispersion y correlación")
             return
         if len(select_dist_item) == 1:
-            fig = go.Figure()
-            fig.add_trace(go.Histogram(
-                x=local_df[select_dist_item[0]], histnorm="probability density", name=select_dist_item[0], marker_color=amazon_palette[0]))
-            x_curve = np.linspace(local_df[select_dist_item[0]].min(
-            ), local_df[select_dist_item[0]].max(), 100)
-            y_curve = stats.norm.pdf(x_curve, np.mean(
-                local_df[select_dist_item[0]]), np.std(local_df[select_dist_item[0]]))
-            fig.add_trace(go.Scatter(x=x_curve, y=y_curve,
-                          mode='lines', name='Normal Curve'))
-            fig.update_yaxes(showticklabels=False)
-            fig.update_layout(
-                title_text=f"Distribucion de {select_dist_item[0]}", xaxis_title_text=select_dist_item[0], showlegend=False)
-
-            fig_box = go.Figure(go.Box(
-                x=local_df[select_dist_item[0]], name=select_dist_item[0], marker_color=amazon_palette[0]))
-
-            st.plotly_chart(fig)
-            st.plotly_chart(fig_box)
-            return
-
-        for item in select_dist_item:
-            fig1 = go.Figure(go.Histogram(
-                x=local_df[item], name=item, marker_color=amazon_palette[0], histnorm="probability density"))
-            x_curve = np.linspace(local_df[item].min(
-            ), local_df[item].max(), 100)
-            y_curve = stats.norm.pdf(x_curve, np.mean(
-                local_df[item]), np.std(local_df[item]))
-            fig1.add_trace(go.Scatter(x=x_curve, y=y_curve,
-                                      mode='lines', name='Normal Curve', line=dict(color=amazon_palette[1])))
-            fig1.update_yaxes(showticklabels=False)
-            fig1.update_layout(
-                title_text=f"Distribucion de {item}", xaxis_title_text=item, showlegend=False)
-            fig1_box = go.Figure(go.Box(
-                x=local_df[item], name=item, marker_color=amazon_palette[0]))
-            fig1_box.update_layout(
-                title_text=f"Boxplot de {item}", xaxis_title_text=item)
-
+            fig1 = px.histogram(
+                local_df[select_dist_item[0]], title=f"Distribución de {select_dist_item[0]}", color_discrete_sequence=amazon_palette)
             st.plotly_chart(fig1)
-            st.plotly_chart(fig1_box)
+            return
+        fig1 = make_subplots(rows=1, cols=2, subplot_titles=[
+            f"Distribución de {select_dist_item[0]}", f"Distribución de {select_dist_item[1]}"])
+        fig1.add_trace(go.Histogram(
+            x=local_df[select_dist_item[0]], name=select_dist_item[0], marker_color=amazon_palette[0]), row=1, col=1)
+        fig1.add_trace(go.Histogram(
+            x=local_df[select_dist_item[1]], name=select_dist_item[1], marker_color=amazon_palette[1]), row=1, col=2)
+        fig1.update_layout(bargap=0.2)
+        fig1.update_layout(
+            title_text=f"Distribucion de {select_dist_item[0]} y {select_dist_item[1]}", showlegend=False)
 
-        st.plotly_chart(px.scatter(x=local_df[select_dist_item[1]], y=local_df[select_dist_item[0]], title=f"Relacion entre {select_dist_item[1]} y {select_dist_item[0]}",
-                        color_discrete_sequence=amazon_palette, trendline_color_override="#ff9900", trendline="ols").update_layout(xaxis_title=select_dist_item[1], yaxis_title=select_dist_item[0]))
+        st.plotly_chart(fig1)
+
+        st.plotly_chart(px.scatter(x=local_df[select_dist_item[0]], y=local_df[select_dist_item[1]], title=f"Relacion entre {select_dist_item[0]} y {select_dist_item[1]}",
+                        color_discrete_sequence=amazon_palette, trendline_color_override="#ff9900", trendline="ols").update_layout(xaxis_title=select_dist_item[0], yaxis_title=select_dist_item[1]))
         with st.expander(label=f"Mas informacion sobre el grafico", icon=":material/thumb_up:"):
             st.markdown(
                 f"#### En el grafico se muestra la correlacion entre el rating y el porcentaje de descuento")
